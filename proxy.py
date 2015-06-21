6ec84a8564eefa25cd92e8151db3cd75a0bd018f
Ation <gation@gmail.com>
2015-06-21 23:26:26 +0300

    Add comparison operators

--- proxy.py
+++ proxy.py
@@ -1,7 +1,6 @@
 
-
 class Proxy:
-    def __init__(self, ip, port, proxy_type):
+    def __init__(self, ip, port=80, proxy_type='HTTP'):
         self._ip = ip
         self._port = port
         self._type = proxy_type
@@ -14,3 +13,12 @@ class Proxy:
 
     def get_type(self):
         return self._type
+
+    def __eq__(self, other):
+        try:
+            return self._ip == other.get_ip() and self._port == other.get_port() and self._type == other.get_type()
+        except:
+            return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
