bc81d923000a9c85152cb036f791c8707ab0249e
Ation <gation@gmail.com>
2015-06-22 12:12:12 +0300

    Add proxy manager class
    Naive single thread implementation. Should be extended to update async while requesting proxy if list is empty.

---
+++ proxymanager.py
@@ -0,0 +1,20 @@
+import collections
+
+class ProxyManager:
+    def __init__(self):
+        self._proxy_list = collections.deque()
+        self._proxy_providers = []
+
+    def get_proxy(self):
+        if len(self._proxy_list) == 0:
+            return None
+
+        return self._proxy_list.pop()
+
+    def add_proxy_provider(self, provider):
+        self._proxy_providers.append(provider)
+
+    def update_proxy_list(self):
+        for provider in self._proxy_providers:
+            self._proxy_list.etend( provider.get_proxy_list() )
+
