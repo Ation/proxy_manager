1784961145e9e28fea7d5d3d41a4bed6429ef252
Ation <gation@gmail.com>
2015-06-27 11:34:37 +0300

    Test proxy list for availability

--- test.py
+++ test.py
@@ -1,6 +1,32 @@
 import proxymanager
 import hidemyass
 
+import requests
+
+def test_proxy(proxy):
+    attempts = 0
+    while attempts < 3:
+        s = requests.Session()
+        if proxy.get_type() == 'HTTP':
+            url = 'http://ya.ru'
+            s.proxies = { 'http': 'http://'+proxy.get_ip()+':'+proxy.get_port() }
+        elif proxy.get_type() == 'HTTPS':
+            url = 'https://ya.ru'
+            s.proxies = { 'https': 'https://'+proxy.get_ip()+':'+proxy.get_port() }
+        else:
+            url = 'http://ya.ru'
+
+        try:
+            r = s.get(url, timeout = 1)
+            if r.status_code == 200:
+                return True
+        except:
+            pass
+
+        attempts += 1
+
+    return False
+
 def main():
     proxy_manager = proxymanager.ProxyManager()
     hma_provider = hidemyass.ProxyProvider()
@@ -9,12 +35,25 @@ def main():
 
     proxy_manager.update_proxy_list('HTTPS')
 
+    working = 0
+    failed = 0
+
     while True:
         proxy = proxy_manager.get_proxy()
         if proxy is None:
             break
 
-        print(proxy.get_ip() + ':' + proxy.get_port() + ' ( ' + proxy.get_type() + ' )' )
+        print('Testing: ' + proxy.get_ip() + ':' + proxy.get_port() + ' ( ' + proxy.get_type() + ' )' )
+        if test_proxy(proxy):
+            print('working')
+            working += 1
+        else:
+            print('failed')
+            failed += 1
+
+
+    print("Failed: " + str(failed))
+    print('Working' + str(working))
 
 if __name__ == '__main__':
     main()
