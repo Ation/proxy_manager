77334bc370b65113c1278dde79c0755732624086
Ation <gation@gmail.com>
2015-06-27 11:51:35 +0300

    Extend testing

--- test.py
+++ test.py
@@ -5,16 +5,36 @@ import requests
 
 def test_proxy(proxy):
     attempts = 0
+
+    http_result = test_http(proxy)
+    https_result = test_https(proxy)
+
+    return http_result, https_result
+
+def test_http(proxy):
+    attempts = 0
     while attempts < 3:
         s = requests.Session()
-        if proxy.get_type() == 'HTTP':
-            url = 'http://ya.ru'
-            s.proxies = { 'http': 'http://'+proxy.get_ip()+':'+proxy.get_port() }
-        elif proxy.get_type() == 'HTTPS':
-            url = 'https://ya.ru'
-            s.proxies = { 'https': 'https://'+proxy.get_ip()+':'+proxy.get_port() }
-        else:
-            url = 'http://ya.ru'
+        url = 'http://ya.ru'
+        s.proxies = { 'http': 'http://'+proxy.get_ip()+':'+proxy.get_port() }
+
+        try:
+            r = s.get(url, timeout = 1)
+            if r.status_code == 200:
+                return True
+        except:
+            pass
+
+        attempts += 1
+
+    return False
+
+def test_https(proxy):
+    attempts = 0
+    while attempts < 3:
+        s = requests.Session()
+        url = 'https://ya.ru'
+        s.proxies = { 'https': 'https://'+proxy.get_ip()+':'+proxy.get_port() }
 
         try:
             r = s.get(url, timeout = 1)
@@ -33,7 +53,7 @@ def main():
 
     proxy_manager.add_proxy_provider(hma_provider)
 
-    proxy_manager.update_proxy_list('HTTPS')
+    proxy_manager.update_proxy_list()
 
     working = 0
     failed = 0
@@ -44,13 +64,14 @@ def main():
             break
 
         print('Testing: ' + proxy.get_ip() + ':' + proxy.get_port() + ' ( ' + proxy.get_type() + ' )' )
-        if test_proxy(proxy):
-            print('working')
+        http_result, https_result = test_proxy(proxy)
+        print ('  HTTP: ' + ' OK' if http_result else 'FAILED')
+        print ('  HTTPS: ' + ' OK' if https_result else 'FAILED')
+
+        if http_result or https_result:
             working += 1
         else:
-            print('failed')
             failed += 1
-
 
     print("Failed: " + str(failed))
     print('Working' + str(working))
